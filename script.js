70c35dcc3dac4e22c59bc513bf8864be7e2db3b2
Robelek <92119683+Robelek@users.noreply.github.com>
2021-10-09 23:25:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let iletile = 16;
+let canvas=[];
+let pierwszeuzycie=true;
+let poprzedninumerpol =0;
+function color(e)
+{
+    
+    e.classList.toggle('tileblack');
+       //alert(e);
+}
+function start()
+{
+    var numerpol = document.getElementById("numerpol").value;
+    //alert(numerpol);
+    if(!pierwszeuzycie)
+    {
+        
+        for(let i=poprzedninumerpol;i>0;i--)
+        {
+            let ostatniousuniete = document.getElementById("canvas").lastElementChild;
+            document.getElementById("canvas").removeChild(ostatniousuniete);
+        }
+        canvas.length=0;
+        pierwszeuzycie=true;
+    }
+    for(let i=0;i<numerpol;i++)
+    {
+        canvas.push(document.createElement("div"));
+        canvas[i].classList.add('tile');
+        canvas[i].onclick = function()
+        {
+        color(this);
+        }
+    document.getElementById("canvas").appendChild(canvas[i]);
+    }
+    poprzedninumerpol=numerpol;
+    pierwszeuzycie=false;
+}
